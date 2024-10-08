a85d179a52162d8a00b69dfa58cb3d93cacba297
Andrei Alisenok <andreialisenok@gmail.com>
2024-10-08 12:32:20 +0300

    fix

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+
+import '../scss/style.scss';
+// import switchThemeColor from './localstaradge.js';
+// switchThemeColor();
+
+// const btn = document.querySelector('.btn');
+// if (btn) {
+//   btn.addEventListener('click', () => {
+//     console.log('click');
+//   });
+// }
