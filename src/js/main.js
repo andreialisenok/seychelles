497068f93d0ddef2bfa9e5f01d47d70630b8249e
Andrei Alisenok <andreialisenok@mdao.team>
2024-10-09 18:19:00 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,15 @@
 import '../scss/style.scss';
+import 'swiper/css';
+
+import '../../node_modules/select2/dist/css/select2.min.css';
+import Swiper from 'swiper';
 
 const burger = document.querySelector('.burger');
 burger.addEventListener('click', () => {});
+
+import select2 from 'select2';
+import $ from 'jquery';
+select2();
+$('#lang').select2();
+
+const swiper = new Swiper('.promo', {});
