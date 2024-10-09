2af1884d38c1a13d3a21d5d529a833108b452b80
Andrei Alisenok <andreialisenok@gmail.com>
2024-10-09 14:09:32 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,3 @@ import '../scss/style.scss';
 
 const burger = document.querySelector('.burger');
 burger.addEventListener('click', () => {});
-
-const inputVal = document.querySelectorAll('.inputVal');
-inputVal.forEach((input) => {
-  input.addEventListener('input', (e) => {
-    console.log(e.target.value);
-    console.log(e);
-  });
-});
